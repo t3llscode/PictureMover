2757edcaa2680e78475a2d92601eda683c0004a2
Tell Hensel <hensel.tell@t-online.de>
2022-12-13 16:34:34 +0100

    V1.3 (unstable)

--- PictureMover.py
+++ PictureMover.py
@@ -5,9 +5,16 @@ from typing import List
 from datetime import datetime
 
 def main():
-    pictureMove = Mover(Essentials.sdCard, Essentials.picPath, Essentials.vidPath, True)
 
-class Essentials:
+    directories = []
+    directories.append(Directory(Es.picPath, Es.picSuffixes))
+    directories.append(Directory(Es.vidPath, Es.vidSuffixes))
+    srcDirectories = []
+    srcDirectories.append(srcDirectory(Es.sdCard, Es.allSuffixes, True))
+
+    # pictureMove = Mover(Es.sdCard, Es.picPath, Es.vidPath, True)
+
+class Es:
 
     picSuffixes = [".ARW", ".JPG", ".dng", ".tif"]
     vidSuffixes = [".XML", ".MP4"]
@@ -25,76 +32,107 @@ class Essentials:
 class File:
 
     path = None
-    filePath = None
     name = None
+    suffix = None
     cDate = None
-    found = False
 
     def __init__(self, _path) -> None:
         self.path = _path.replace("\\", "\\\\")
-        self.filePath = self.checkIfPic()
         self.name = os.path.basename(self.path)
+        self.suffix = os.path.splitext(self.path)[-1]
         self.cDate = datetime.fromtimestamp(os.path.getctime(self.path))
-        
-    def checkIfPic(self):
-        suffix = os.path.splitext(self.path)[1]
-        if suffix in Essentials.picSuffixes:
-            return Essentials.picPath
-        elif suffix in Essentials.vidSuffixes:
-            return Essentials.vidPath
-        else:
-            print(f"[WARNING] {os.path.basename(self.path)} is in an unknown File Format")
-            return Essentials.errPath
-        
-class Mover:
 
-    searchDir = None
-    picDir = None
-    vidDir = None
-    searchSub = True
-    files = []
+class Directory:
 
-    def __init__(self, _searchDir, _picDir, _vidDir, _searchSub) -> None:
-        self.directory = _searchDir
-        self.picDir = _picDir
-        self.vidDir = _vidDir
-        self.searchSub = _searchSub
-        self.files = self.findFiles()
-        self.createFolders()
-        self.moveFiles()
-
-    def findFiles(self): 
-        tmp = []  
-        for suffix in Essentials.allSuffixes:
-            for path in glob.glob(self.directory + "**\\*" + suffix, recursive = self.searchSub):
-                tmp.append(File(path))
-        return tmp
-
-    def createFolders(self):
-        tmp = []
-        dateList = [[], []] # 0 = pic # 1 = vid
-        for file in self.files:
-            if file.filePath == Essentials.picPath:
-                tmp = [dateList[0]]
-            elif file.filePath == Essentials.vidPath:
-                tmp = [dateList[1]]
-            tmp[0].append(file.cDate.strftime("%Y-%m-%d"))
-        for d in range(len(dateList)):
-            for date in list(dict.fromkeys(dateList[d])):
-                if d == 0:
-                    tmp = [self.picDir, "Picture Directory"]
-                elif d == 1:
-                    tmp = [self.vidDir, "Video Directory"]
-                try:    
-                    os.makedirs(tmp[0] + date, exist_ok = False)
-                    print(f"[LOG] Created {tmp[1]} \"{date}\"")
-                except:
-                    print(f"[WARNING] {tmp[1]} \"{date}\" already existing")
+    path = None
+    acceptedSuffixes = None
     
-    def moveFiles(self):
-        for file in self.files:
-            shutil.copy2(file.path, file.filePath + file.cDate.strftime("%Y-%m-%d\\") + file.name)
-            print(f"[LOG] {file.name} was moved to " + file.filePath + file.cDate.strftime("%Y-%m-%d\\") + file.name)
+    def __init__(self, _path, _acceptedSuffixes) -> None:
+        self.path = _path
+        self.acceptedSuffixes = _acceptedSuffixes
+
+    def createDir(self):
+        try:    
+            os.makedirs(self.path, exist_ok = False)
+            print(f"[LOG] Created directory {self.path}")
+        except:
+            print(f"[WARNING] Directory {self.path} already existing")
+
+class dstDirectory(Directory):
+
+    files = None
+    
+    def __init__(self, _path, _acceptedSuffixes) -> None:
+        super().__init__(_path, _acceptedSuffixes)
+        self.files = self.updateFiles()
+        
+
+    def updateFiles(self):    # Test if its faster to search for files with suffix, or search for all and filter those with suffix
+        tmp = []
+        for suffix in self.acceptedSuffixes:
+            for path in glob.glob(self.path + "**\\*" + suffix, recursive = self.includeSub):
+                tmp.append(File(path))
+        self.files = tmp
+
+class srcDirectory(dstDirectory):
+
+    includeSub = None
+
+    def __init__(self, _path, _acceptedSuffixes, _includeSub) -> None:
+        super().__init__(_path, _acceptedSuffixes)
+        self.includeSub = _includeSub
+
+
+
+# class Mover:
+
+#     searchDir = None
+#     picDir = None
+#     vidDir = None
+#     searchSub = True
+#     files = []
+
+#     def __init__(self, _searchDir, _picDir, _vidDir, _searchSub) -> None:
+#         self.directory = _searchDir
+#         self.picDir = _picDir
+#         self.vidDir = _vidDir
+#         self.searchSub = _searchSub
+#         self.files = self.findFiles()
+#         self.createFolders()
+#         self.moveFiles()
+
+    # def findFiles(self): 
+    #     tmp = []  
+    #     for suffix in Essentials.allSuffixes:
+    #         for path in glob.glob(self.directory + "**\\*" + suffix, recursive = self.searchSub):
+    #             tmp.append(File(path))
+    #     return tmp
+
+#     def createFolders(self):
+#         tmp = []
+#         dateList = [[], []] # List containing the dates the pics / vids were created # 0 = pic # 1 = vid
+#         for file in self.files:
+#             if file.filePath == Essentials.picPath:
+#                 tmp = [dateList[0]]
+#             elif file.filePath == Essentials.vidPath:
+#                 tmp = [dateList[1]]
+#             tmp[0].append(file.cDate.strftime("%Y-%m-%d"))
+#         for d in range(len(dateList)):
+#             for date in list(dict.fromkeys(dateList[d])):
+#                 if d == 0:
+#                     tmp = [self.picDir, "Picture Directory"]
+#                 elif d == 1:
+#                     tmp = [self.vidDir, "Video Directory"]
+                # try:    
+                #     os.makedirs(tmp[0] + date, exist_ok = False)
+                #     print(f"[LOG] Created {tmp[1]} \"{date}\"")
+                # except:
+                #     print(f"[WARNING] {tmp[1]} \"{date}\" already existing")
+    
+#     def moveFiles(self):
+#         for file in self.files:
+#             shutil.copy2(file.path, file.filePath + file.cDate.strftime("%Y-%m-%d\\") + file.name)
+#             print(f"[LOG] {file.name} was moved to " + file.filePath + file.cDate.strftime("%Y-%m-%d\\") + file.name)
 
 if __name__=="__main__":
     main()
